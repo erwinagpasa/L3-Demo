eb46691f1cc4c211f1f7f72d8edff23e9e840495
erwinagpasa <rootaccess000@gmail.com>
2024-02-28 10:20:34 +0300

    Initial Commit

---
+++ L3Demo/L3DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L3DemoApp.swift
+//  L3Demo
+//
+//  Created by Developer on 28/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct L3DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
