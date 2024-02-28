914e066cf4db29084ed6f3b933600ac283dbcbe0
erwinagpasa <rootaccess000@gmail.com>
2024-02-28 14:11:35 +0300

    finalizing the card

--- L3Demo/ContentView.swift
+++ L3Demo/ContentView.swift
@@ -9,43 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-      VStack (alignment: .leading, spacing: 20){
-
-        Image("london")
-         .resizable()
-         .aspectRatio(contentMode:.fit)
-         .cornerRadius(15)
+      ZStack {
+        Color(.systemMint)
+          .ignoresSafeArea()
         
-        HStack {
-          Text("Street Walk")
-            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+        VStack (alignment: .leading, spacing: 20) {
+          Image("london")
+           .resizable()
+           .aspectRatio(contentMode:.fit)
+           .cornerRadius(15)
           
-          Spacer()
-          
-          VStack{
-            HStack{
-              Image(systemName: "star.fill")
-              Image(systemName: "star.fill")
-              Image(systemName: "star.fill")
-              Image(systemName: "star.fill")
-              Image(systemName: "star.leadinghalf.fill")
+          HStack {
+            Text("Street Walk")
+              .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+              .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            
+            Spacer()
+            
+            VStack{
+              HStack{
+                Image(systemName: "star.fill")
+                Image(systemName: "star.fill")
+                Image(systemName: "star.fill")
+                Image(systemName: "star.fill")
+                Image(systemName: "star.leadinghalf.fill")
+              }
+              Text("Reviews 345")
             }
-            Text("Reviews 345")
+            .foregroundColor(.orange)
+            .font(.caption)
+            
           }
-          .foregroundColor(.orange)
-          .font(.caption)
           
-        }
-        
-        Text ("Amazing London Street Walk taken in 2024!")
-        HStack{
-          Image(systemName: "fork.knife")
-          Image(systemName: "binoculars.fill")
+          Text ("Amazing London Street Walk taken in 2024!")
+          HStack{
+            Spacer()
+            Image(systemName: "fork.knife")
+            Image(systemName: "binoculars.fill")
+          }
+          .foregroundColor(.gray)
+          .font(.caption)
 
         }
-
+        .padding()
+        .background(Rectangle().foregroundColor(.white)
+          .cornerRadius(15)
+          .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+        )
+        .padding()
       }
+      
+
+      
 
     }
 }
